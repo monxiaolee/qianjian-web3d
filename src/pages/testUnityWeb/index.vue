12d9197b5943e4ff3182757ab3f3733bbb1e15cc
limengxiao <limengxiao@qianxin.com>
2023-07-29 22:35:49 +0800

    feat: test

--- src/pages/testUnityWeb/index.vue
+++ src/pages/testUnityWeb/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="unity-web__wrapper">
-        <canvas id="unity-canvas" width=1920 height=1080 style="width: 1920px; height: 1080px; background: #231F20"></canvas>
+        <iframe width="100%" height="100%" frameborder="none" scrolling="no" src="http://127.0.0.1"></iframe>
     </div>
 </template>
 <script>
@@ -12,14 +12,19 @@ export default {
         }
     },
     mounted() {
-        var buildUrl = 'Build';
-        var loaderUrl = buildUrl + '/WebGL.loader.js'
+        window.addEventListener('fn', this.unityWatch)
+    },
+    methods: {
+        unityWatch(e) {
+            console.log('-------', e)
+        }
     }
+    
 }
 </script>
 <style lang="scss" scoped>
 .unity-web__wrapper{
-    width: 100%;
-    height: 100%;
+    width: 1920px;
+    height: 1080px;
 }
 </style>
